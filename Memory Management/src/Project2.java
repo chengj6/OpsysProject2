15dfbe795fe45b25b764514b7e563563bfa40f16
ShashankSundar <sundas6@rpi.edu>
2018-05-02 11:14:16 -0400

    Getting somewhere here
    Midway through finishing next fit

--- Memory Management/src/Project2.java
+++ Memory Management/src/Project2.java
@@ -29,9 +29,8 @@ public class Project2 {
 		
 		ArrayList<Process> processes = new ArrayList<Process>();
 		ArrayList<Character> memory = new ArrayList<Character>(Collections.nCopies(Max_Mem_Frames, '.'));
-		
 		String st;
-		while ((st = br.readLine()) != null) {
+		while ( (st = br.readLine()) != null) {
 			if (st.equals(""))
 				continue;
 			int newMark = 0;
@@ -58,6 +57,7 @@ public class Project2 {
 				runTimes.add(Integer.parseInt(st.substring(oldMark, newMark)));
 			}
 		}
+		next_fit(processes, memory, null);
 		
 	}
 	
@@ -73,19 +73,71 @@ public class Project2 {
 		System.out.println("================================");
 	}
 	
-	private static void defragment(ArrayList<Character> memory){
-		
+	private static void defragment(ArrayList<Character> memory, ArrayList<Process> activeProc){
+		for (int i = 0; i < Max_Mem_Frames; i++) {
+			if (memory.get(i) != '.') {
+				int currentMemSpace = 0;
+				for (int k = 0; k < activeProc.size(); k++) {	//Check which process we are dealing with
+					if (activeProc.get(k).getID().charAt(0) == memory.get(i)) {
+						currentMemSpace = activeProc.get(k).getMemFrames();
+						break;
+					}
+				}
+				
+				int freeSpace = 0;
+				for (int j = i; j > 0; j--) {
+					if (memory.get(j-1) == '.')
+						freeSpace++;
+					else {
+						memory.subList(i-freeSpace, i).clear();
+						ArrayList<Character> addFreeSpace = new ArrayList<Character>(Collections.nCopies(freeSpace, '.'));
+						memory.addAll(i, addFreeSpace);
+						freeSpace = 0;
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	private static void addNextFit(Process process, ArrayList<Character> memory, ArrayList<Process> activeProc){
+		int spaceNeeded = process.getMemFrames();
+		int freeSpace = 0;
+		int loop = 0;
+		for (int i = justPlaced; i < Max_Mem_Frames; i++) {
+			if (memory.get(i) == '.') {
+				freeSpace++;
+				if(freeSpace >= spaceNeeded) {
+					for (int j = justPlaced; j < justPlaced+spaceNeeded; j++) {
+						memory.set(j, process.getID().charAt(0));
+					}
+					justPlaced += spaceNeeded;
+					System.out.println("time "+time+"ms: Placed process "+process.getID());
+					return;
+				}
+			}
+			else {
+				freeSpace = 0;
+			}
+			
+			if (i == Max_Mem_Frames-1 && loop == 0) {
+				i = -1;
+				loop = 1;
+				freeSpace = 0;
+				justPlaced = 0;
+			}
+		}
+		defragment(memory, activeProc);
 	}
 	
 	public static void arrival(ArrayList<Process> processes, ArrayList<Character> memory, ArrayList<Process> activeProc) {
 		for(int i=0; i<processes.size();i++) {
 			ArrayList<Integer> arrTimes = processes.get(i).getArrTimes();
 			int currentBurst = processes.get(i).getBLA();
-			
-			if (arrTimes.get(currentBurst) == time) {
+			if (currentBurst<arrTimes.size() && arrTimes.get(currentBurst) == time) {
 				activeProc.add(processes.get(i));
-				System.out.println("time "+time+"ms: Process "+processes.get(i).getID()+" arrived (required "+processes.get(i).getMemFrames()+" frames)");
-				//addNextFit(processes.get(i), memory);
+				System.out.println("time "+time+"ms: Process "+processes.get(i).getID()+" arrived (requires "+processes.get(i).getMemFrames()+" frames)");
+				addNextFit(processes.get(i), memory, activeProc);
 				printMemory(memory);
 			}
 		}
@@ -104,7 +156,7 @@ public class Project2 {
 			ArrayList<Integer> arrTimes = activeProc.get(i).getArrTimes();
 			ArrayList<Integer> runTimes = activeProc.get(i).getRTimes();
 			int currentBurst = activeProc.get(i).getBLA();
-			if(time == arrTimes.get(currentBurst)+runTimes.get(currentBurst)){
+			if(currentBurst<arrTimes.size() && time == arrTimes.get(currentBurst)+runTimes.get(currentBurst)){
 				System.out.println("time "+time+"ms: Process "+ activeProc.get(i).getID()+" removed");
 				activeProc.get(i).incrementBLA(); //need to add way to make sure this doesn't go out of bounds
 				remove(activeProc.get(i), memory);
@@ -121,6 +173,7 @@ public class Project2 {
 		while (true) {
 			arrival(processes, memory, activeProcesses);
 			removal(activeProcesses, memory);
+			time++;
 		}
 		
 	}
